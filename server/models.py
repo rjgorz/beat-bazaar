ddab7046c024c6b511db2765cb08b0c61da1e175
Robert Gorz <rjgorz@gmail.com>
2023-03-29 12:42:45 -0500

    initial commit

---
+++ server/models.py
@@ -0,0 +1,43 @@
+from sqlalchemy_serializer import SerializerMixin
+from sqlalchemy.ext.associationproxy import association_proxy
+
+from config import db
+
+# Models go here!
+class Song(db.Model, SerializerMixin):
+    __tablename__ = 'songs'
+
+    id = db.Column(db.Integer, primary_key=True)
+    title = db.Column(db.String, nullable=False)
+    likes = db.Column(db.Integer, nullable=False)
+    genre = db.Column(db.String, nullable=False)
+    url = db.Column(db.String, nullable=False)
+
+    playlist_songs = db.relationship('PlaylistSong', backref='song')
+
+    def __repr__(self):
+        return f'<Song {self.title}>'
+    
+
+class Playlist(db.Model, SerializerMixin):
+    __tablename__ = 'playlists'
+
+    id = db.Column(db.Integer, primary_key=True)
+    title = db.Column(db.String, nullable=False)
+
+    playlist_songs = db.relationship('PlaylistSong', backref='playlist')
+
+    def __repr__(self):
+        return f'<Playlist {self.title}>'
+    
+class PlaylistSong(db.Model, SerializerMixin):
+    __tablename__ = 'playlist_songs'
+
+    id = db.Column(db.Integer, primary_key=True)
+    user = db.Column(db.String, nullable=False)
+
+    song_id = db.Column(db.Integer, db.ForeignKey('songs.id'))
+    playlist_id = db.Column(db.Integer, db.ForeignKey('playlists.id'))
+
+    def __repr__(self):
+        return f'<PlaylistSong {self.user}>'
