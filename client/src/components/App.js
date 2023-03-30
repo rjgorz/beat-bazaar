23317669372ccf818da59ede731a312fea411d4b
Robert Gorz <rjgorz@gmail.com>
2023-03-30 09:28:02 -0500

    testing

--- client/src/components/App.js
+++ client/src/components/App.js
@@ -2,30 +2,55 @@ import React, { useEffect, useState } from "react";
 import { Switch, Route } from "react-router-dom";
 import Playlists from "./Playlists";
 import AllSongs from "./AllSongs";
-import Nav from "./Nav";
+import Navigation from "./Nav";
+import '../index.css'
 import Home from "./Home";
-
 
 function App() {
   const [playlists, setPlaylists] = useState([])
   const [songs, setSongs] = useState([])
+  const [theme, setTheme] = useState('dark')
+
+  const toggleTheme = () => {
+    if (theme === 'light') {
+      setTheme('dark');
+    } else {
+      setTheme('light');
+    }
+  };
 
   useEffect(() => {
     fetch('/songs')
-    .then(r => r.json())
-    .then(song_data => setSongs(song_data))
-    
+      .then(r => r.json())
+      .then(song_data => setSongs(song_data))
+
     fetch('/playlists')
-    .then(r => r.json())
-    .then(playlist_data => setPlaylists(playlist_data))
+      .then(r => r.json())
+      .then(playlist_data => setPlaylists(playlist_data))
   }, [])
 
-  return(
+  useEffect(() => {
+    document.body.className = theme;
+  }, [theme]);
+
+  
+
+  function addSong() {
+    return
+  }
+
+  return (
     <div>
-      <Nav />
+      <div style={{
+        backgroundImage: `url("https://www.onlygfx.com/wp-content/uploads/2018/08/10-realistic-lightning-bolt-cover.jpg")`
+      }} className={`App ${theme}`}>
+        <button className="btn" style={{ float: 'right' }} onClick={toggleTheme}>Toggle Theme</button>
+        <h1 className='h1'>BeatBazaar</h1>
+        <Navigation style={{ float: 'right' }} />
+      </div>
       <Switch>
         <Route exact path='/'>
-          <Home />
+          <Home addSong={addSong} />
         </Route>
         <Route path='/playlists'>
           <Playlists playlists={playlists} />
@@ -34,7 +59,6 @@ function App() {
           <AllSongs songs={songs} />
         </Route>
       </Switch>
-      <Playlists playlists={playlists} />
     </div>
   )
 }
--- client/src/components/Home.js
+++ client/src/components/Home.js
@@ -1,9 +1,13 @@
 import React from 'react';
+import SongForm from './SongForm'
 
-function Home() {
-
-    return(
-        <div></div>
+function Home({addSong}) {
+    return (
+        <div style={{
+            backgroundImage: `url("https://www.onlygfx.com/wp-content/uploads/2018/08/10-realistic-lightning-bolt-cover.jpg")`
+        }}>
+            <SongForm addSong={addSong} />
+        </div>
     )
 }
 
